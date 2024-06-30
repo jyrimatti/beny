ef04d0ffbbb042adce8fde783a0e3499466ca3fd
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-06-30 22:48:27 +0300

    seems to need locking for parallel commands

--- beny_cmd.sh
+++ beny_cmd.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i dash --pure --keep CREDENTIALS_DIRECTORY -I channel:nixos-23.11-small -p dash coreutils xxd netcat bc
+#! nix-shell -i dash --pure --keep CREDENTIALS_DIRECTORY -I channel:nixos-23.11-small -p dash coreutils xxd netcat bc flock
 
 cmd="$1"
 
 . ./beny_env.sh
 
-echo -n "${BENY_PREFIX}$cmd" | xxd -r -p | nc -u -W1 "$BENY_HOST" "$BENY_PORT" | xxd -p
+echo -n "${BENY_PREFIX}$cmd" | xxd -r -p | flock ./beny_env.sh nc -u -W1 "$BENY_HOST" "$BENY_PORT" | xxd -p
