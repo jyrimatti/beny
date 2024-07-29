5ca921e079b346184c9bf6c0f29233e2bb636487
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-07-29 19:31:58 +0300

    hmph...

--- cmd/plugged.sh
+++ cmd/plugged.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ret="$(dash ./beny_cmd.sh "3062303030316532343037303964" | tr -d '\n' | head -c 84 | tail -c 1)"
-if [ "$ret" = "3" ]; then
+if [ "$ret" = "3" ] || [ "$ret" = "1" ]; then
     echo 0
 else
     echo 1
