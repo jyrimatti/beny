d477e43a81e49687cda95b543358a0a5f3a84bc5
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-07-29 17:47:12 +0300

    whoops

--- beny_env.sh
+++ beny_env.sh
@@ -6,9 +6,10 @@ export BENY_PORT="$(cat "${CREDENTIALS_DIRECTORY:-.}/.beny-port")"
 export BENY_PREFIX="3535616130303030"
 
 convert() {
-  read -r x
-  a16="$x / 100"
-  a10="$x % 100 / 10 / 3 - 1"
-  a1="$x % 10"
-  echo "($a16) * 16 + ($a10) * 10 + ($a1) - ($a10)" | bc
+  while read -r x; do
+    a16="$x / 100"
+    a10="$x % 100 / 10 / 3 - 1"
+    a1="$x % 10"
+    echo "($a16) * 16 + ($a10) * 10 + ($a1) - ($a10)" | bc
+  done
 }
